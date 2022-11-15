20467726a6767e1383d71d4bd3b9985a77d6172c
renata-aline <renataaline@gmail.com>
2022-11-15 14:00:26 -0300

    alterei a class pessoa

--- objetos/lista-exercicios-objeto-imc.js
+++ objetos/lista-exercicios-objeto-imc.js
@@ -5,41 +5,44 @@ Instancie uma pessoa chamada josé que tenha 70kg de peso
 e 1,75 de altura e peça ao Jose para dizer o valor do seu IMC;
 */
 
-class pessoa {
+class Pessoa {
     nome;
     peso;
     altura;
 
-    constructor (nome, peso, altura) {
+    constructor(nome, peso, altura) {
         this.nome = nome;
         this.peso = peso;
         this.altura = altura;
+    }
 
-}
-calcularImc(){
-  return this.peso / (this.altura * this.altura);
-}
+    setPeso(peso){
+        this.peso = peso;
+    }
 
-classificarImc() {
-    const imc = this.calcularImc();
-    if (imc < 18.5) {
-        return 'abaixo do peso';
-    }else if (imc >=18.5 && imc < 25) {
-        return 'peso normal';
-    }else if (imc >=25 && imc < 30){
-        return 'acima do peso';
-    }else if (imc >=30 && imc < 40) {
-        return 'obeso';
-    }else {
-        return'obesidade grave';
+    calcularImc() {
+        return this.peso / (this.altura * this.altura);
+    }
+
+    classificarImc() {
+        const imc = this.calcularImc();
+        if (imc < 18.5) {
+            return 'abaixo do peso';
+        } else if (imc >= 18.5 && imc < 25) {
+            return 'peso normal';
+        } else if (imc >= 25 && imc < 30) {
+            return 'acima do peso';
+        } else if (imc >= 30 && imc < 40) {
+            return 'obeso';
+        } else {
+            return 'obesidade grave';
+        }
     }
 }
 
-
-
-}
-
-const jose = new pessoa('jose', 70, 1.75);
+const jose = new Pessoa('jose', 70, 1.75);
 console.log(jose.classificarImc());
-const Re = new pessoa('Re', 50, 1.53);
+const Re = new Pessoa('Re', 50, 1.53);
 console.log(Re.classificarImc());
+Re.setPeso(Re.peso - 1);
+console.log(Re.peso);

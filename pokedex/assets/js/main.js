37d606653a3bd784bd7bc96dbf1d0a5e5f56e994
renata-aline <renataaline@gmail.com>
2022-11-26 14:58:03 -0300

    utilizaçao da função map

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -2,10 +2,10 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonToLi(pokemon) {
+function convertPokemonToLi(name, img, id) {
     return (`<li class="pokemon">
-        <span class="number">#001</span>
-        <span class="name">${pokemon.name}</span>
+        <span class="number">#${id}</span>
+        <span class="name">${name}</span>
 
         <div class="detail">
             <ol class="types">
@@ -13,20 +13,41 @@ function convertPokemonToLi(pokemon) {
                 <li class="type">poison</li>
 
             </ol>
-            <img src= "https://raw.githubusercontent.com/pokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt= "${pokemon.name}">
+            <img src="https://raw.githubusercontent.com/pokeAPI/sprites/master/sprites/pokemon/other/dream-world/${img}.svg" alt="${name}">
         </div>
     </li>`)
 }
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = []
+pokeApi.getPokemons().then((pokemons = []) => {
 
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+        
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /*const listItems = [];
+    
     for (let i = 0; i < pokemons.length; i++) {
+        
         const pokemon = pokemons[i];
-        listItems.push(convertPokemonToLi(pokemon))
+        pokemon.img = i + 1;
+        pokemon.id = i + 1;
+        
+        listItems.push(convertPokemonToLi(pokemon.name, pokemon.img, pokemon.id))
     }
     
-    pokemonList.innerHTML  = listItems;
-});
+    pokemonList.innerHTML  = listItems.join('');
+})*/

6839b05891298bb5c0529df7c6c53998ba780fa1
renata-aline <renataaline@gmail.com>
2022-11-28 21:18:45 -0300

    convertendo poke-api para novo modelo

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,35 +1,27 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) =>`<li class="type"> ${typeSlot.type.name}</li>`)
-}
-
+// const offset = 0;
+// const limit = 10;
+// const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon) {
-    return (`<li class="pokemon">
-        <span class="number">#${pokemon.order}</span>
+    return `<li class="pokemon">
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-            ${convertPokemonTypesToLi(pokemon.types).join('') }
-
+                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
             </ol>
-            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
-    </li>`)
+    </li>`
 }
 
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-        
-    })
-
+    const newHTML = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHTML
+})
 
 
 
@@ -42,16 +34,17 @@ pokeApi.getPokemons().then((pokemons = []) => {
 
 
 
-   /*const listItems = [];
-    
-    for (let i = 0; i < pokemons.length; i++) {
-        
-        const pokemon = pokemons[i];
-        pokemon.img = i + 1;
-        pokemon.id = i + 1;
-        
-        listItems.push(convertPokemonToLi(pokemon.name, pokemon.img, pokemon.id))
-    }
-    
-    pokemonList.innerHTML  = listItems.join('');
+
+/*const listItems = [];
+ 
+ for (let i = 0; i < pokemons.length; i++) {
+     
+     const pokemon = pokemons[i];
+     pokemon.img = i + 1;
+     pokemon.id = i + 1;
+     
+     listItems.push(convertPokemonToLi(pokemon.name, pokemon.img, pokemon.id))
+ }
+ 
+ pokemonList.innerHTML  = listItems.join('');
 })*/

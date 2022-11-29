71c0f264e15dd9426b27ca5deb9ecf3f15b75813
renata-aline <renataaline@gmail.com>
2022-11-28 22:28:36 -0300

    adicionando tipos de pokemon

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -3,13 +3,13 @@
 // const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon) {
-    return `<li class="pokemon">
+    return `<li class="pokemon ${pokemon.type}">
         <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                ${pokemon.types.map((type) => `<li class="type" ${type}>${type}</li>`).join('')}
             </ol>
             <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
